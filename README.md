84856817f19ca9306e29a8fef624a1b8fe954de6
Hellovie <hellovie@foxmail.com>
2024-02-15 19:32:37 +0800

    feat(all): 搭建DDD项目环境

---
+++ README.md
@@ -0,0 +1,11 @@
+# Snapvids
+
+**项目名称：** 短视频 APP 服务端
+
+**作者：** Hellovie
+
+**邮箱：** hellovie@foxmail.com
+
+**网站：** https://hellovie.github.io
+
+---
---
+++ snapvids-server-launcher/src/main/java/io/github/hellovie/snapvids/launcher/MainApplication.java
@@ -0,0 +1,18 @@
+package io.github.hellovie.snapvids.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序主入口。
+ *
+ * @author hellovie
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
